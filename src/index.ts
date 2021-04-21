3ac658bb04b402c8cb0f621568d29ecb74264466
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-04-21 21:43:26 +0200

    🚀 shelljs: now it checks if it can works with git commands

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
 
 import fs from 'fs';
 import chalk from 'chalk'
+import shell from 'shelljs'
 import inquirer from 'inquirer'
-import questions from './utils/questions'
+import trainingQuestions from './utils/trainingQuestions'
+import commitQuestions from './utils/commitQustions'
 import data from './data'
 import TrainingData from './types/TrainingData'
+import CommitData from './types/CommitData'
+
+
+if (!shell.which('git')) {
+  chalk.red("no git program found.")
+  shell.exit(1);
+}
 
 inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 
 console.log(chalk.green("Hello guy 🦄, The program has just started"))
 
 async function init(){
-  const trainingData : TrainingData = await inquirer.prompt(questions)
+  const commitData: CommitData = await inquirer.prompt(commitQuestions)
+  const trainingData: TrainingData = await inquirer.prompt(trainingQuestions)
 
   if(trainingData) {
     const result: TrainingData[] = [...data, trainingData ]
     console.log(chalk.green(`The new data is now available with ${result.length} results`))
     fs.writeFileSync("./src/data.ts", `export default ${JSON.stringify(result, null, 2)}`) 
   }
+
 }
 
 init()
---
+++ src/utils/commitQustions.ts
@@ -0,0 +1,6 @@
+export default [{
+  name: "commit",
+  type: "checkbox",
+  message: "would you like to commit these training?",
+  choices: [{name:"yes", value: true}, {name: "no", value: false}]
+}]
