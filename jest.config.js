0055af7d31d76a9b66bc622df6ddd89035930eda
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-04-21 21:23:10 +0200

    🔧 jest semplified

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,8 @@ module.exports = {
   "roots": [
     "<rootDir>/src"
   ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
+  "testMatch": ["**/?(*.)+(test).+(ts|js)"],
   "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    "^.+\\.(ts)$": "ts-jest"
   },
 }
